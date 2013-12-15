c2c98d31cea1c6dd2d8d5124effce44241b83590
Tyrel Richey <tyrelrichey@gmail.com>
2013-12-15 01:11:43 -0700

    Asumes directory

--- bson_to_json/convert_all.rb
+++ bson_to_json/convert_all.rb
@@ -1,4 +1,4 @@
 Dir["*.bson"].each do |file|
 	base_name = file.split(".").first
-	system "cat #{file} | ruby convert.rb > #{base_name}.json"
+	system "cat #{file} | ruby /~/Code/ruby/utils/Convert.rb > #{base_name}.json"
 end
